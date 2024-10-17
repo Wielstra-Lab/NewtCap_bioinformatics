3e936cc9a2cc4c2b4935c7cd6cb08ddfb5fd5918
Manon de Visser <63111175+vissermcde@users.noreply.github.com>
2024-10-17 15:18:16 +0200

    Create 4_run_vcf2phylip_and_ascbias.sh
    Create 4_run_vcf2phylip_and_ascbias.sh

---
+++ RAxML_analyses/4_run_vcf2phylip_and_ascbias.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --ntasks=
+#SBATCH --cpus-per-task=
+#SBATCH --partition=
+#SBATCH --mem=
+#SBATCH --output=
+#SBATCH --error=
+#SBATCH --job-name=
+#SBATCH --mail-type=
+#SBATCH --mail-user=
+
+
+# Change to directory where the msgVCF is located that contains the samples of interest for a certain phylogenetic analysis
+cd /PATH/TO/VARIANTS/
+
+# Load required modules (see GitHub respositories highlighted in line 22 and 29 for more information)
+numpy
+pandas
+Biopython
+
+# Run 'vcf2phylip.py' (see; Ortiz, 2019 - https://github.com/edgardomortiz/vcf2phylip)
+# This code is written as a for loop, in case multiple different msgVCF files are worked up for phylogenetic analyses at once (but obviously the command can be run on it's own for one file separately too) 
+for file in *_miss05.vcf
+do
+ python3 /PATH/TO/vcf2phylip.py --input $file
+done
+
+# Run 'ascbias.py' (see - https://github.com/btmartin721/raxml_ascbias)
+# This code is written as a for loop, in case multiple different msgVCF files are worked up for phylogenetic analyses at once (but obviously the command can be run on it's own for one file separately too) 
+# Note that the 'min4*.phy' file is the output of the ‘vcf2phylip.py’ script 
+for file in *min4*.phy
+do
+  python3 /PATH/TO/ascbias.py -p $file -o ${file%.phy}.unInv.phy
+done
