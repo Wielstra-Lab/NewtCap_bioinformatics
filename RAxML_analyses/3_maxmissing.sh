712142a907fc63a3b829210d2738ba3883b346d7
Manon de Visser <63111175+vissermcde@users.noreply.github.com>
2024-10-17 15:01:57 +0200

    Update 3_maxmissing.sh
    removed whitelines

--- RAxML_analyses/3_maxmissing.sh
+++ RAxML_analyses/3_maxmissing.sh
@@ -17,7 +17,6 @@
 cd /PATH/TO/VARIANTS/
 
 # This code is written as a for loop, in case multiple different msgVCF files are worked up for phylogenetic analyses at once (but obviously the command can be run on it's own for one file separately too)
-
 for file in *SNPs*PassOnly.vcf
 do
   /PATH/TO/vcftools --vcf $file --max-missing 0.5 --recode --recode-INFO-all --out ${file%.vcf}_miss05.vcf
