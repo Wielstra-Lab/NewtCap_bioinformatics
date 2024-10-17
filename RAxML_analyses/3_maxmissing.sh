2a784ee3535b5255b83f3924daaa127c6e2097eb
Manon de Visser <63111175+vissermcde@users.noreply.github.com>
2024-10-17 15:20:17 +0200

    Update 3_maxmissing.sh
    changed directions

--- RAxML_analyses/3_maxmissing.sh
+++ RAxML_analyses/3_maxmissing.sh
@@ -13,7 +13,7 @@
 # In our studies, we usually filter out sites with more than 50% missing data for PCA and RAxML analyses  
 # For 50% filtering you have to set the value to 0.5 (and for instance for 100% filtering you would set the value to 1.0, which would mean no missing genotypes/data points would remain at all after filtering)
 
-# Change to directory where the msgVCF is located that contains the samples of interest for a certain phylogenetic analysis
+# Change to directory where the file with the high quality SNPs is located that contains the samples of interest for a certain phylogenetic analysis
 cd /PATH/TO/VARIANTS/
 
 # This code is written as a for loop, in case multiple different msgVCF files are worked up for phylogenetic analyses at once (but obviously the command can be run on it's own for one file separately too)
